6af5a001f00e146596cacaa179eb1f1b5bedc8b0
mhydara0624 <mhydara0624@icloud.com>
2021-08-10 16:06:22 -0400

    moles, timer and score all working wooohhooo!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const boxes = document.querySelectorAll('.box')
-const timer = document.querySelector('.time')
-const score = document.querySelector('.score')
+const timer = document.querySelector('#time')
+const score = document.querySelector('#score')
 const mole = document.querySelector('.mole')
 let currentScore = 0
 let timeLeft = 60
@@ -11,50 +11,35 @@ function addMole() {
   boxes.forEach((box) => {
     box.classList.remove('mole')
   })
-
-  let randomBox = boxes[Math.floor(Math.random() * 9)]
+  let randomPosition = Math.floor(Math.random() * 9)
+  let randomBox = boxes[randomPosition]
   randomBox.classList.add('mole')
-  randomBox.id = molePosition
-  console.log(molePosition)
+  console.log(randomBox.id)
+  molePosition = randomBox.id
 }
 addMole()
 
-// function moleMovements() {
-//   moleActive = setInterval(addMole, Math.random() * 1500)
-// }
-// moleMovements()
+function moleMovements() {
+  moleActive = setInterval(addMole, Math.random() * 1500)
+}
+moleMovements()
+
 function timeTicking() {
   timeLeft--
   timer.innerText = timeLeft
 
   if (timeLeft == 0) {
     clearInterval(countDownTimer)
+    clearInterval(moleActive)
   }
 }
 countDownTimer = setInterval(timeTicking, 1000)
 
-// function stopMole() {
-//   if (timeLeft == 0) {
-//     clearInterval(moleActive)
-//   }
-// }
-// stopMole()
-// function() {
-//   let molePosition = randomBox.id
-//   if (randomBox.id == molePosition) {
-//     currentScore++
-//     score.innerText = currentScore
-//     console.log(currentScore)
-//   }
-// }
-// whackMole()
-
-// boxes.forEach((box) => {
-//   box.addEventListener('click', function () {
-//     // let molePosition = randomBox.id
-//     if (box.id == molePosition) {
-//       currentScore++
-//       // console.log(currentScore)
-//     }
-//   })
-// })
+boxes.forEach((box) => {
+  box.addEventListener('click', function () {
+    if (box.id == molePosition) {
+      currentScore += 10
+      score.innerText = currentScore
+    }
+  })
+})
